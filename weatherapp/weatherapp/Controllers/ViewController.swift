c0db95d78d41be5b6e51f6e821d25c7abbd4e784
ezgidivarci <ezgidivarci1@gmail.com>
2022-07-28 22:21:46 +0300

    first commit

---
+++ weatherapp/weatherapp/Controllers/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by elif nur dıvarcı on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var btn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        btn.layer.cornerRadius = 6
+            
+        
+        
+    }
+    
+    //uygulama ilk açıldıgında gorunen nav controller.ı saklamak
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    @IBAction func didButtonTapped(_ sender: Any) {
+        
+        let city = cityTextField.text ?? ""
+        
+        if city.isEmpty {
+            let alert = UIAlertController(title: "Uyarı", message: "Lütfen bir şehir giriniz.", preferredStyle: .alert)
+            
+            let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alert.addAction(cancelButton)
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        }
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
